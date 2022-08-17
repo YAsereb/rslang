4357dd5ccb4e7c606b210c59fe767dbd42a885a6
YAsereb <cjiaba123@gmail.com>
2022-08-17 18:41:41 +0300

    build: add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,14 @@ const baseConfig = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
   resolve: {
