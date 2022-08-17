53a5928906de0669cda1972d93203e25a86eabeb
YAsereb <cjiaba123@gmail.com>
2022-08-17 22:01:07 +0300

    fit: add main-page layout

---
+++ src/components/main-page/listener.ts
@@ -0,0 +1,3 @@
+const electronicBookButton = document.querySelector('#electronic-book') as HTMLElement;
+const listOfWordsButton = document.querySelector('#list-of-words') as HTMLElement;
+const listAudiaGameButton = document.querySelector('#audio-game') as HTMLElement;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import renderAuthentication from './components/Authentication';
+import { renderFooter, renderMain } from './components/main-page/render';
 
 function start() {
+  renderMain();
+  renderFooter();
   renderAuthentication();
 }
 
