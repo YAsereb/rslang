cc05cfab1aedc8b12d97d621d042aefca47ae8f2
YAsereb <cjiaba123@gmail.com>
2022-08-20 09:55:32 +0300

    fix: fix render footer layout

--- src/components/main-page/compomemts/footer/footer.ts
+++ src/components/main-page/compomemts/footer/footer.ts
@@ -4,26 +4,28 @@ import './footer.scss';
 
 function renderFooter() {
   return `
+  <footer class="footer">
     <div class="RSschool">
       <a href="https://rs.school/js/">
-      <img src="${rsschoolIcon}" alt="School-icon">
+        <img src="${rsschoolIcon}" alt="School-icon">
       </a>
     </div>
     <div class="developer">
       <a href="https://github.com/YAsereb">Serebrenikov Yaroslav
-      <img src="${gitHubIcon}" class="gitHub-icon" alt="gitHub-icon">
+        <img src="${gitHubIcon}" class="gitHub-icon" alt="gitHub-icon">
       </a>
     </div>
     <div class="developer">
       <a href="https://github.com/SEvk4a">
-      <img src="${gitHubIcon}" class="gitHub-icon" alt="gitHub-icon">
+        <img src="${gitHubIcon}" class="gitHub-icon" alt="gitHub-icon">
       </a>
     </div>
     <div class="developer">
       <a href="https://github.com/zhentosmak">
-      <img src="${gitHubIcon}" class="gitHub-icon" alt="gitHub-icon">
+        <img src="${gitHubIcon}" class="gitHub-icon" alt="gitHub-icon">
       </a>
     </div>
+  </footer>
   `;
 }
 
