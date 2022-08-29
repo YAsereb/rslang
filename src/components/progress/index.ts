3022ee2bf84464e5e8729adbe7c301f0626efa3f
YAsereb <cjiaba123@gmail.com>
2022-08-29 19:15:33 +0300

    refactor: add return in function

--- src/components/progress/index.ts
+++ src/components/progress/index.ts
@@ -1,5 +1,9 @@
-function getNewWord() {
+export async function isNewWord() {
+  return Boolean
+}
 
+export async function progressWord() {
+  return { word.guessCount, word.totalAttempt }
 }
 
 /* В игре получать правильное слово и его id делать запрос в agregate words и проверять поле сколько раз оно было записано, и сколько раз угадано. Этот прогресс выводить в карточку слова в учебнике в виде сколько раз угоадано / сколько раз записано.
